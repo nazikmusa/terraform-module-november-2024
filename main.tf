daee93e11409c28e76f1c9241568279b28d038ba
nazikmusa <musaevan331@gmail.com>
2025-02-01 23:03:56 +0000

    subnet added

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource aws_vpc main {
     cidr_block = var.vpc_cidr
 }
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr 
+
+}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,3 @@
 variable region {}
 variable vpc_cidr {}
+variable subnet_cidr {}
